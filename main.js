1875e7a07e051efb41ae618771928c525ad275e5
Florencia Benevenia <fbenevenia@frba.utn.edu.ar>
2022-04-20 21:25:35 -0300

    Ejercicios 1 a 5

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("Bienvenido a la consola de JS");
+document.write("<h1>Hello World</h1>"); //punto 2
+console.log(2);
+console.log(2+2);
+console.log("edad:", 25)//no hace falta cerrar este comentario;
+console.log("Florencia"," ","Benevenia");
+console.log("Florencia"+" "+"Benevenia");
+var PrimerDato;
+
+
+alert("Hello World"); //punto 1
+
+var A = 3
+var B = 5
+var suma = A+B
+document.write(suma+"<br>");//punto 3
+
+var nombre = prompt("Ingrese su nombre de usuario")
+document.write("Hola " + nombre); //punto 4
+
+var edad1 = parseInt(prompt("Ingrese su edad"))
+var edad2 = parseInt(prompt("Ingrese su nro favorito"))
+var suma2 = edad1 + edad2
+document.write("<br>" + suma2); //punto 5
